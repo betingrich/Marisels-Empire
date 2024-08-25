8bfc22609389ac8d890d6074691d9c6b9b2b474f
marisel <140407389+betingrich@users.noreply.github.com>
2024-08-25 23:37:07 +0300

    Update menu-toggle.js

--- scripts/menu-toggle.js
+++ scripts/menu-toggle.js
@@ -1,15 +1,10 @@
+// scripts/menu-toggle.js
+
 document.addEventListener('DOMContentLoaded', () => {
     const menuIcon = document.getElementById('menu-toggle');
     const navMenu = document.getElementById('nav-menu');
 
     menuIcon.addEventListener('click', () => {
         navMenu.classList.toggle('show');
-    });
-
-    // Optional: Close menu when clicking outside of it
-    document.addEventListener('click', (event) => {
-        if (!menuIcon.contains(event.target) && !navMenu.contains(event.target)) {
-            navMenu.classList.remove('show');
-        }
     });
 });

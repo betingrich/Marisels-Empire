0954c46bcf912d4eb965f9de6db33d318d3c3116
marisel <140407389+betingrich@users.noreply.github.com>
2024-08-25 22:54:46 +0300

    Create settings.js

---
+++ scripts/settings.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+    darkModeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+});

0d10565abbdf47daec97dc7c2db7dd9a09639bd9
marisel <140407389+betingrich@users.noreply.github.com>
2024-08-26 00:36:14 +0300

    Update menu-toggle.js

--- scripts/menu-toggle.js
+++ scripts/menu-toggle.js
@@ -1,13 +1,17 @@
-function toggleMenu() {
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.toggle('active');
-}
-
-// Close the menu if clicking outside of it
-document.addEventListener('click', function (event) {
-    const navMenu = document.getElementById('nav-menu');
-    const menuIcon = document.querySelector('.menu-icon');
-    if (!menuIcon.contains(event.target) && !navMenu.contains(event.target)) {
-        navMenu.classList.remove('active');
+document.addEventListener('DOMContentLoaded', function() {
+    function toggleMenu() {
+        const navMenu = document.getElementById('nav-menu');
+        navMenu.classList.toggle('active');
     }
+
+    const menuIcon = document.querySelector('.menu-icon');
+    menuIcon.addEventListener('click', toggleMenu);
+
+    // Close the menu if clicking outside of it
+    document.addEventListener('click', function(event) {
+        const navMenu = document.getElementById('nav-menu');
+        if (!menuIcon.contains(event.target) && !navMenu.contains(event.target)) {
+            navMenu.classList.remove('active');
+        }
+    });
 });
